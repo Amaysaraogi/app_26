8e1860a7c67f5d5bb96323ae43d4e5c297935811
Amay Saraogi <saraogiamay@gmail.com>
2026-03-07 17:47:26 -0800

    finally please

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:app_26/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
+  
 }
 
 class MyApp extends StatelessWidget {
@@ -12,12 +14,13 @@ class MyApp extends StatelessWidget {
     // TODO: implement build
     Widget build(BuildContext context){
       return MaterialApp(
-        title: 'Demo',
+        title: 'Waste sorter',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: HomePage(),
+        home: const HomePage(),
       );
     }
   }
